8138778532367c09159a21b59a042e92f7db5220
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-11-23 11:29:14 +0530

    "Starting with DOM"

---
+++ dom.js
@@ -0,0 +1,19 @@
+// console.log(document.URL);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello";
+// console.log(document);
+// console.log(document.links);
+//console.log(document.forms[0]);
+document.title = "FirstDOMAssignment";
+
+let headerEl = document.getElementById("main-header");
+headerEl.style.borderBottom = "3px solid #000";
+let mainContainer = document.getElementById("main");
+//console.log(mainContainer.firstChild);
+let addItemsEl = mainContainer.firstElementChild;
+addItemsEl.style.fontWeight = "bold";
+addItemsEl.style.color = "rgb(0,255,0)";
