3bf07a29c3bd9e5121f911497483322eef57ff9d
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-11-23 14:48:29 +0530

    QuerySelector

--- dom.js
+++ dom.js
@@ -29,10 +29,24 @@ document.title = "FirstDOMAssignment";
 // }
 
 // *** GETELEMENTSBYTAGNAME *** //
-let li = document.getElementsByTagName("li");
-//console.log(items[0]);
-li[1].textContent = "Hello";
-li[1].style.fontWeight = "bold";
-for (let item of li) {
-  item.style.backgroundColor = "#f4f4f4";
-}
+// let li = document.getElementsByTagName("li");
+// //console.log(items[0]);
+// li[1].textContent = "Hello";
+// li[1].style.fontWeight = "bold";
+// for (let item of li) {
+//   item.style.backgroundColor = "#f4f4f4";
+// }
+
+// *** QUERYSELECTOR *** //
+let header = document.querySelector("#main-header");
+header.style.borderBottom = "4px solid #ccc";
+let inputEl = document.querySelector("input");
+inputEl.value = "Hello World!";
+let submitBtn = document.querySelector('input[type="submit"]');
+submitBtn.value = "SEND";
+let firstItem = document.querySelector(".list-group-item");
+let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.backgroundColor = "#00ff00";
+let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.display = "none";
+let lastItem = document.querySelector(".list-group-item:last-child");
